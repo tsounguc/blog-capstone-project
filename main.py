b8cb54dc4548cd077ee4762ba7337ad0eefc32f1
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-11-28 16:43:26 -0500

    completed capstone project

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
 app = Flask(__name__)
 
-blog_data = []
+blog_url = "https://api.npoint.io/31f3edd52b60ab8cee6e"
+response = requests.get(url=blog_url)
+response.raise_for_status()
+blog_data = response.json()
+posts = []
+
+for post in blog_data:
+    post_object = Post(post['id'], post['title'], post['subtitle'], post['body'])
+    posts.append(post_object)
 
 
 @app.route('/')
 def home():
-    global blog_data
-    blog_url = "https://api.npoint.io/31f3edd52b60ab8cee6e"
-    response = requests.get(url=blog_url)
-    response.raise_for_status()
-    blog_data = response.json()
-    return render_template("index.html", blogs=blog_data)
+
+    return render_template("index.html", blogs=posts)
 
 
 @app.route('/post/<int:blog_id>')
 def post_page(blog_id):
-    global blog_data
-    blog = blog_data[blog_id]
-    print(blog_data)
-    print(blog)
-    return render_template("post.html", blog=blog)
+    requested_post = None
+    for post in posts:
+        if post.id == blog_id:
+            requested_post = post
+    return render_template("post.html", blog=requested_post)
 
 
 if __name__ == "__main__":
